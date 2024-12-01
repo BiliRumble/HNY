31ff8f84228de2019adc4a6d8ba57f48919c6775
BiliRumble <bilirumble@outlook.com>
2024-12-01 10:42:26 +0800

    fix: 未删除的引用

--- src/pages/[...notFound].vue
+++ src/pages/[...notFound].vue
@@ -3,14 +3,10 @@
 		<img width="200px" height="200px" class="logo" src="/src/assets/notFound.svg" />
 		<br />
 		<h1 class="m-10">404 NotFound</h1>
-		<p>此页面位于 <code>src/pages/[...notFound].vue</code></p>
-		<switchDark class="m-15" />
 	</div>
 </template>
 
-<script setup lang="ts">
-import SwitchDark from '@/components/SwitchDark/index.vue';
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 .wrap {
